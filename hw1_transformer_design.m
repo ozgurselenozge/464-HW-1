8f9270e24a0672d26e89529396582bcb1f5d43b5
onatsimsek <147510645+onatsimsek@users.noreply.github.com>
2024-03-23 23:26:43 +0300

    transformer is designed

--- hw1_transformer_design.m
+++ hw1_transformer_design.m
@@ -5,7 +5,7 @@ Dmax = 12.75/32.75;
 fs = 100e3; % Hz
 Vsmin = 20; % V
 Vsmax = 40; % V
-Bmax = 0.1761; % T
+Bmax = 0.2; % T
 permeability_core = 115;
 permeability_air = 4 * pi * 10^-7;
 effective_area_core = 125e-6; % B66363G0500X187 % m^2
@@ -23,12 +23,12 @@ I_in_RMS = sqrt((peak_current^2 + min_current^2 + peak_current * min_current)*Dm
 I_in_avg = (peak_current + min_current) * Dmax / 2;
 efficiency = 60/P_in;
 resistivity_of_copper = 1.68e-5; 
-surface_area_awg_26 = 0.140; % mm
+surface_area_awg_26 = 0.140; % mm^2
 
 fprintf ('peak_current = %f\n', peak_current) 
 fprintf ('delta_I = %f\n', delta_I)
 
-Lm = ((Vsmin * Dmax)^2) / ((2 * P_in) * fs * ripple_factor);
+Lm = ((Vsmax * Dmin)^2) / ((2 * P_in) * fs * ripple_factor);
 fprintf('Lm = %f\n', Lm);
 disp(Lm)
 
@@ -38,7 +38,7 @@ fprintf('min_prim_winding_turns = %f\n', min_prim_winding_turns);
 primary_turns = sqrt(Lm / A_L);
 primary_turns = round(primary_turns);
 fprintf('primary_turns= %f\n', primary_turns)
-necessary_primary_turns = primary_turns + 1; 
+necessary_primary_turns = primary_turns; 
 LM = necessary_primary_turns^2 * A_L;
 disp(LM)
 Lm_final = necessary_primary_turns^2 * A_L;
@@ -54,7 +54,7 @@ length_of_cable = 69*necessary_primary_turns; % mm
 
 number_of_cables_needed = I_in_RMS / 0.361;
 fprintf('number_of_cables_needed = %f\n', number_of_cables_needed)
-number_of_cables_needed_final = 22;
+number_of_cables_needed_final = 20;
 
 R_awg_26 = resistivity_of_copper * length_of_cable / surface_area_awg_26 / number_of_cables_needed_final;
 fprintf('R_awg_26 = %f\n', R_awg_26);
